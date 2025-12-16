5cdc684ca872a0c7dffe940f7bcb77fcd885fb80
Bilal <saritasbilal67@gmail.com>
2025-12-16 15:18:12 +0300

    feat: add stylesheet link in index.html and enhance App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,27 @@ import React, { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
-  
+    return(
+        <div style={{padding:'20px'}}>
+            <h1>Kampüs Radarı 📍</h1>
+            <p style={{fontFamily:'arial'}}>Burası proje üssümüz Burda: <br />
+                Popüler sorunlar <br/>
+                Kaç sorun paylaştın <br/>
+                ...
+                </p>
+            {/*Ileride harita buray gelcek*/}
+            <div tsyle={{
+                widht:'80%',
+                height:'400px',
+                bacgroundColor:'gray',
+                display:'flex',
+                alignitems:'center',
+                justifyContent:'center',
+            }}>
+                Harita burda gözükecek
+            </div>
+        </div>
+    )
 }
 
 export default App;
