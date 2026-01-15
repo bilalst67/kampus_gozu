5b9157b9aa3db1b97dd8702c6e0687345e369387
e-ukiyo <87203515+hacywhatt@users.noreply.github.com>
2026-01-15 22:49:13 +0300

    Refactor routes to update middleware and add public access
    Updated middleware paths and added public routes for users and problems.

--- server/routes/usroutes.js
+++ server/routes/usroutes.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const problemController = require('../controllers/problemcontroller')
 const userController = require('../controllers/userController');
-const verifyToken = require("../middleware/authmiddleware");
-const verifyAdmin = require("../middleware/adminMiddleware"); // EKLENDİ
+const verifyToken = require("../security/authmiddleware");
+const verifyAdmin = require("../security/adminMiddleware");
+const upload = require('../security/upload');
 
-router.get('/users', verifyToken, userController.getUsers);
-router.get('/users/:id', verifyToken, userController.getUserById);
+// Public Rotalar (Herkes Erişebilir)
+router.get('/public/users', userController.getPublicUserList);
+router.get('/public/problems',problemController.getPublicProblems)
 router.post('/newuser', userController.createUser);
 router.post('/login', userController.login);
-router.put('/update/:id', verifyToken, userController.updateUser);
 
-// SİLME İŞLEMİ: Hem giriş yapmış olmalı (Token) hem de Admin olmalı
+// Korumalı Rotalar (Sadece Giriş Yapanlar)
+router.get('/user/problem/full', verifyToken,problemController.fullProblem);
+router.get('/users', verifyToken, userController.getUsers);
+router.get('/users/:id', verifyToken, userController.getUserById);
+router.get('/user/:id', verifyToken, problemController.userreq);
+router.get('/notifications/:userId', verifyToken, problemController.getUserNotifications);
+router.get('/admin/problems', verifyToken, verifyAdmin, problemController.getAllProblemsForAdmin);
+
+// Sorun işlemleri
+router.post('/user/newproblem', verifyToken, upload.single('Fotograf'), problemController.userCreateProblem);
+router.post('/user/problem/support/:id', verifyToken, problemController.toggleSupport);
+router.delete('/problem/delete/:id', verifyToken, problemController.userDeleteProblem);
+
+// Kullanıcı Güncelleme ve Silme
+router.put('/update/:id', verifyToken, userController.updateUser);
+router.put('/admin/problem/status/:id', verifyToken, verifyAdmin, problemController.updateProblemStatus);
 router.delete('/delete/:id', verifyToken, verifyAdmin, userController.deleteUser);
+router.delete('/admin/problem/:id',verifyToken,verifyAdmin,problemController.deleteProblemAdmin)
 
 module.exports = router;
